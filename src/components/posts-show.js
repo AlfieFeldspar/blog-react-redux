0031ec141937cb42f0e16bbda66a2a16c7780868
Aissa Feldmann <aissaf@umich.edu>
2020-09-15 15:54:12 -0400

    first commit

---
+++ src/components/posts-show.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchPost } from "../actions";
+
+class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h1>{post.title}</h1>
+        <h3>{post.categories}</h3>
+        <h3>{post.content}</h3>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchPost}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
