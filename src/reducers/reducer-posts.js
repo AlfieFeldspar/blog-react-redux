119413cdfa2933a05366f1cb777a1c17fae2110a
Aissa Feldmann <aissaf@umich.edu>
2020-09-16 11:28:06 -0400

    wired up with redux form for new posts

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,13 +1,14 @@
-import _ from "lodash";
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST, CREATE_POST } from '../actions';
 
-export default function(state = {}, action) {
+export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_POST:
-      debugger;
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, "id");
+      return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
